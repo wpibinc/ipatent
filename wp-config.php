e7a79bfba309c0dba78dd5426815951990f7848a
yedidel <yedidel@a06ca754-9de1-4799-a813-a206a7e2237e>
2013-07-26 13:30:42 +0000

    Fix

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'ipatent_wmg');
 
 /** MySQL database username */
-define('DB_USER', 'dev');
+define('DB_USER', 'ipatent_wmg');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'nakujho');
+define('DB_PASSWORD', 'M45nv2qA');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
