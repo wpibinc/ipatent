04db4221863bbed73aec37338bc649e0461d8c3f
yedidel <yedidel@a06ca754-9de1-4799-a813-a206a7e2237e>
2013-09-11 22:19:50 +0000

    Task #13: תיקון TITLE Task #6: תיקון תפריט עליון רמה שלישית

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -45,7 +45,7 @@
 <meta name="viewport" content="width=device-width" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-<title><?php wp_title( '|', true, 'right' ); echo 'Erlich & Fenster - Patent & Trademark attorney'; ?>'); ?></title>
+<title><?php wp_title( '|', true, 'right' );?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
