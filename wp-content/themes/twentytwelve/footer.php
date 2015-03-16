8668348c902f42017f8da9eb3f34e4dc091f7163
serg <serg@a06ca754-9de1-4799-a813-a206a7e2237e>
2015-03-16 13:50:00 +0000

    Task #33 - hide search bar on iOS less then 5

--- wp-content/themes/twentytwelve/footer.php
+++ wp-content/themes/twentytwelve/footer.php
@@ -66,7 +66,20 @@
 	</div>
 </footer><!-- #colophon -->
 <script>
+	function iOSversion() {
+		if (/iP(hone|od|ad)/.test(navigator.platform)) {
+			// supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+			var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+			return parseFloat([parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)]);
+		}
+	}
+
 $(function() {
+		var verIOS = iOSversion();
+		if(verIOS && verIOS < 5){
+			$('.mobile-searchform').hide();
+		}
+
 	   $('li.menu-item').click(function() {
 	      window.location.href = $('a', this).attr('href');
 	      return false;
